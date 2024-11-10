7f4d42174edd1ce3bb4eec4c4725fd6d5986dd24
Izulsyauki Imani <izulsyaukiimani@gmail.com>
2024-11-10 16:50:18 +0700

    update axios and env

--- src/app/libs/axios.ts
+++ src/app/libs/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const API = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: import.meta.env.VITE_API_URL || "https://haloa-app-backend.vercel.app/api",
 });
 
 API.interceptors.request.use(
